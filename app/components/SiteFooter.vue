cb107277332bfe31b856034f78627684987e90ab
XmasSiYuan <h823353440@163.com>
2025-07-05 20:27:50 +0800

    ok

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -31,8 +31,8 @@
         {{ $t("footer.fiveMinutes") }}
       </n-p>
       <n-p depth="3">
-        Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        Copyright &copy; 2025 - {{ new Date().getFullYear() }}
+        <n-text depth="3" @click="jumpLink(linkData.home)"> SiYuan </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/xmassiyuan",
+  home: "https://nekoyuan.top",
+  email: "mailto:syxmas@nekoyuan.top",
 };
 </script>
 
